63ef6025c8b7991e1765b4702fbc25214c9871bc
PCMaster2165 <chrisbrett@shaw.ca>
2017-04-02 11:57:31 -0700

    Servo code

--- main.c
+++ main.c
@@ -7,7 +7,74 @@
 #include <avr/io.h>
 #include "UART/BlockingUART.h"
 #include "sensor/joystick.h"
+#include "servo.h"
+
+int panPos = 208;
+int tiltPos = 208;
+int laserOn = 0; //0 is Off, 1 is On
+int laserPin = 30;
+
+void tiltUp() {
+	int foo=0; while(foo>0){
+		servo_pen_up();
+		foo--;
+	}
+}
+
+void panLeft() {
+	tiltPos-=2;
+	servo_set(tiltPos);
+}
+
+void tiltDown() {
+  int foo=0; while(foo>0){
+		servo_pen_down();
+		foo--;
+	}
+}
+
+void panRight() {
+    tiltPos+=2;
+	servo_set(tiltPos);
+}
+
+void switchLaser() {
+	if(laserOn==1){
+		laserOn=0
+		toggle_LED_C7();
+	}else{
+		laserOn=1;
+		toggle_LED_C7();
+	}
+}
+
+void servo(){
+	servo_init();
+	while(1){
+		int command = UART_Recieve1();
+		switch(command){
+			case: 'U': //tilt up
+			tiltUp();
+			break;
+			case: 'D': //tilt down
+			tiltDown();
+			break;
+			case: 'L': //pan left
+			panLeft();
+			break;
+			case: 'R': //pan right
+			panRight();
+			break;
+			case: 'Z':
+			switchLaser();
+			break;
+		}
+		Task_Next();
+	}
+}
 
 void a_main(void) {
+	UART_Init1(9600);
+	Task_Create_Period(0,25,10,6);
 }
 
